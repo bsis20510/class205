05ff05f2bb85e62b2bc04fdce3a94768b352231f
apple <apple@apple-ui-MacBook-Pro-39.local>
2014-03-05 14:06:54 +0900

    Initial Commit

---
+++ 20510/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  20510
+//
+//  Created by apple on 14. 3. 5..
+//  Copyright (c) 2014년 apple. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+

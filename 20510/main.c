bb03d55e27df29200546a54d7e56a741ca13dd72
apple <apple@apple-ui-MacBook-Pro-39.local>
2014-03-11 15:27:36 +0900

    Mar 3rd

--- 20510/main.c
+++ 20510/main.c
@@ -8,11 +8,25 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
+int main()
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    /*
+    printf("Hello");
+    printf("Hello\n");
+    printf("Hello\nTom");
+    
+    printf("2+2=4\n");
+    printf("2+2=%d\n",2*2);
+    printf("%d+%d=%d\n",2,2,4);
+    printf("r=3, area=%f",3*3*3.14);
+    */
+    
+    printf("Tom! %s\n","hello");
+    printf("Initial is %c.%c.%c\n",'P','S','Y');
+    int eqfwefwefwewfqeqwweqfeqwfewfeqwfefwe ;
+    eqfwefwefwewfqeqwweqfeqwfewfeqwfefwe=10;
+    int hello=10;
+    printf("%d\n",hello);
     return 0;
 }
 

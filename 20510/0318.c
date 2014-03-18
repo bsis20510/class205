f8daa3eb5c183baddcd8eba80e8346232d01da88
apple <apple@apple-ui-MacBook-Pro-39.local>
2014-03-18 16:15:55 +0900

    3.18 4 20’

---
+++ 20510/0318.c
@@ -0,0 +1,14 @@
+//
+//  0318.c
+//  20510
+//
+//  Created by apple on 14. 3. 18..
+//  Copyright (c) 2014년 apple. All rights reserved.
+//
+
+#include <stdio.h>
+int main(){
+    
+    printf("hi!");
+    return 0;
+}
--- 20510/FirstClass.c
+++ 20510/FirstClass.c
@@ -7,45 +7,14 @@
 //
 
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    /*
-    printf("Hello");
-    printf("Hello\n");
-    printf("Hello\nTom");
-    
-    printf("2+2=4\n");
-    printf("2+2=%d\n",2*2);
-    printf("%d+%d=%d\n",2,2,4);
-    printf("r=3, area=%f",3*3*3.14);
-    */
-    
-//    printf("Tom! %s\n","hello");
-//    printf("Initial is %c.%c.%c\n",'P','S','Y');
-//    int eqfwefwefwewfqeqwweqfeqwfewfeqwfefwe ;
-//    eqfwefwefwewfqeqwweqfeqwfewfeqwfefwe=10;
-//    int hello=10;
-//    printf("%d\n",hello);
-//    int a=10;
-//    char c='T', s[10]="Tom";
-//    printf("a=%d, a의 주소는 %d\n",a,&a);
-//    printf("a=%d, a의 주소는 %x\n",a,&a);
-//    printf("%s's initial is %c\n",s,c);
-    //traning no.1
-//    int x=0,y=0;
-//    printf("(%d,%d) ->",x,y);
-//    x=x+5;
-//    y=y-10;
-//    printf(" (%d,%d)\n",x,y);
-
-    int x1,y1,x2,y2;
-    x1=0;
-    y1=0;
-    x2=2;
-    y2=3;
-    printf("(%d,%d) 부터(%d,%d)까지의 거리는 %f 입니다. \n",x1,y1,x2,y2,sqrt(pow((x1-x2),2)+pow((y1-y2), 2)));
+    int r;
+    double pi;
+    printf("반지름 값과 원주율 값을 입력하시오. ex) 3,4.15:");
+    scanf("%d,%lf",&r,&pi);
+    printf("원의 넓이는 %.3f입니다",r*r*pi);
     return 0;
 }
 

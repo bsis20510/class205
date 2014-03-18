a2c11828c1f214b8360581d844256894e6a5586c
apple <apple@apple-ui-MacBook-Pro-39.local>
2014-03-18 16:56:45 +0900

    3 18 end

--- 20510/FirstClass.c
+++ 20510/FirstClass.c
@@ -10,11 +10,24 @@
 
 int main()
 {
+/*
     int r;
     double pi;
     printf("반지름 값과 원주율 값을 입력하시오. ex) 3,4.15:");
     scanf("%d,%lf",&r,&pi);
     printf("원의 넓이는 %.3f입니다",r*r*pi);
-    return 0;
-}
+
+    char firstName,middleName;
+    printf("First Initial: ");
+    scanf("%c\n",&firstName);
+    printf("Middle initial: ");
+    scanf("%c", &middleName);
+    printf("%c.%c\n",firstName,middleName);
+*/
+    char str[20];
+    printf("Your name:");
+    gets(str);
+    printf("Name: %s\n",str);
+    printf("Last name's inital %c\n",str[0]);
+ }
 
